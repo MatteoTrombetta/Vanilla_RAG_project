55796b22ee948ac7af014807e03e689115190014
MatteoTrombetta <matteo.trombetta07@gmail.com>
2026-03-03 11:54:01 +0100

    DocumentProcessor fixed and should be working, to test still

--- src/DocumentProcessor.py
+++ src/DocumentProcessor.py
@@ -20,32 +20,37 @@ Non usare un Vector DB già pronto, ma salva i vettori in un array Numpy.
 
 
 from pathlib import Path
-import os
-import Document
 
 class DocumentProcessor:
+    """Class to handle the processing and the split of the document into chunks"""
+    def __init__(self, chunk_size: int=500, chunk_overlap: int=100) -> None:
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
 
-    def __init__(self, chunck_size=1000, chunck_overlap=200):
-        self.chunck_size = chunck_size
-        self.chunck_overlap = chunck_overlap
+    def load_document(self, file_path: Path) -> str:
+        return file_path.read_text(encoding='utf-8')      
+    
+    def chunks_text(self, doc: str) -> list[str]:
+        """Method used to split the document into chunks
+        mimics a sliding window to allow overlapping between chunks"""
+        # POSSIBLE FUTURE IMPROVEMENT:
+        # We could use regex or 'nltk' library to allow dynamic splits at the end of phrases or paragraphs.
+        chunks: list[str] = []
+        # range(start, stop, step)
+        for i in range(0, len(doc), self.chunk_size-self.chunk_overlap):
+            block: str = doc[i:i+self.chunk_size]
+            chunks.append(block)
+        return chunks
 
-    def load_document(self, file_path):
-        # file_path = Path.cwd() # current working directory
-        # __file__ gives the path of the current script file
-        file_path = Path(__file__).parent.resolve() #.parent gets the containing folder, .resolve() returns an absolute path
-        print('File Path: ', file_path)
-        # Remeber that here the path is on '/docs' check whether you need to change that
-        try:
-            with (file_path / 'docs/file_prova2.txt', 'r') as file:
-                content = file.read()
-                print(content) # can be removed
-        except FileNotFoundError:
-            print("Error: file does not exists")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-        
-        return content
-
+"""             if i-self.chunk_overlap <= 0:
+                block: str = doc[0:self.chunk_size + self.chunk_overlap]
+            elif i+self.chunk_overlap > len(doc):
+                block: str = doc[i:]
+            else:
+                block: str = doc[i:self.chunk_size + self.chunk_overlap]
+            chunks.append(block) 
+            return chunks
+"""
 
 
 
