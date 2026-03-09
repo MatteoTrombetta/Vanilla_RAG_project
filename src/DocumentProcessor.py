46eff451636ced6626d7a741661ba3902d3527f9
MatteoTrombetta <matteo.trombetta07@gmail.com>
2026-03-09 10:31:43 +0100

    Minor changes in classes

--- src/DocumentProcessor.py
+++ src/DocumentProcessor.py
@@ -1,20 +1,3 @@
-'''
-"""Class to handle the processing and the split of the document into chunks"""
-
-1. Scrivi una classe Python che legge un file PDF o TXT e lo divide in blocchi (chunking) usando logica pura (es. contando i token o i caratteri).
-
-2. Usa le API (es. OpenAI o un modello locale con Ollama) per trasformare questi blocchi in vettori numerici (Embeddings). 
-Non usare un Vector DB già pronto, ma salva i vettori in un array Numpy.
-
-3. Scrivi tu la funzione matematica in Python per calcolare la Cosine Similarity tra la domanda dell'utente e i tuoi documenti.
-
-4. Passa il contesto trovato all'LLM per fargli generare la risposta.
-
-'''
-
-# Let's start by creating a class that handles the preprocessing part from a PDF file
-
-
 from pathlib import Path
 
 class DocumentProcessor:
